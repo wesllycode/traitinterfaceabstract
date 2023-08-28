b205fed25dec3a6c034cdc2aa015d22416692089
wesllycode <wesllycode07@gmail.com>
2023-08-27 21:10:49 -0300

    Usando classe abstract, interface e trait usando tudo e cada um com sua finalidade

--- src/Concert.php
+++ src/Concert.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'src/HasMenu.php';
 require_once 'src/Event.php';
+require_once 'src/HasAssigneSeats.php';
+require_once 'src/PricingContract.php';
+require_once 'src/SeatingContract.php';
 class Concert extends Event implements PricingContract, SeatingContract
 {
     use HasMenu;
